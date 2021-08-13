1100ff9fcd247a21fb72038fd72b9503653ba3b9
Diyorbek5 <davlatovdiyorbek15@gmail.com>
2021-08-13 15:28:56 +0500

    z

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -12,16 +12,6 @@ const onChiqish = ()=>{
         <h1 style={{textAlign: 'center'}}>
             <Button type='primary' onClick={onChiqish}><Link to='/'>Chiqish</Link></Button>
         </h1>
-        <Navbar bg="dark" variant="dark">
-            <Container>
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
     )
 }
 
