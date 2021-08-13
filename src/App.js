9718f02f17376c23c3f44fe520e42f6b536178be
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-08-13 11:01:13 +0500

    insert selectt

--- src/App.js
+++ src/App.js
@@ -1,92 +1,91 @@
-import React, {useState, useEffect} from 'react';
+import React,{useState, useEffect} from 'react';
 import {YMaps, Map, Clusterer, Placemark,   TypeSelector, ZoomControl, GeolocationControl, RouteButton, TrafficControl } from 'react-yandex-maps'
 import { Points} from './server'
 import RingLoader from 'react-spinners/RingLoader'
 import Dialog from './components/Dialog'
-import Typography from '@material-ui/core/Typography';
-import pin from './boy.png'
+//import pin from './boy.png'
 import 'antd/dist/antd.css'
 import './App.css'
 import Select from './components/Select'
-function App() {
-    const [points,setPoints]= useState('')
-    const [loading,setLoading]= useState(true)
-    const [forclick,setforclick]= useState(false)
-    const [village, setVillage] = useState('')
-    const [formouse,setformouse] = useState(true)
-    const [name,setName] = useState('')
 
-    useEffect(()=>{
-        setPoints(Points)
-        setLoading(false)
-    },[])
-    const onstart = (e) => {
-      setformouse(false)
-      setName(e)
-    };
-    const onclose = () => {
-      setformouse(true)
-    };
-    
-    const Information = (e)=>{
-      setforclick(true)
-      setVillage(e)
+function App() {
+     const [loading,setLoading]= useState(true)
+     const [forclick,setforclick]= useState(false)
+     const [village, setVillage] = useState('')
+     const [mapBool,setMapBool] = useState(false)
+     const [param, setParam]= useState([41.311151, 69.279716])
+     const [zoom,setZoom] = useState(6)
+     useEffect(()=>{
+         setLoading(false)
+     },[zoom])
+
+    const Information = ()=>{
+      const dataJson = localStorage.getItem('selectNeighborhood')
+      const data = JSON.parse(dataJson)
+      setforclick(true)  
+      setVillage(data[0])
+    }
+    const handleParam = ()=>{
+      let param = localStorage.getItem('param')
+      setParam(JSON.parse(param))
+      setZoom(14)
+      const dataJson = localStorage.getItem('selectNeighborhood')
+      const data = JSON.parse(dataJson)
+      console.log(data)
+      if(data){
+        setMapBool(true)
+      }else{
+        setMapBool(false)
+      }
     }
     const handleClose = () => {
       setforclick(false);
     };
       return (
     <>
-    {loading ? (
+     {loading ? (
       <div style={{width: '100vw', height: '100vh', display:'flex', justifyContent: 'center', alignItems: 'center' }}>
         <RingLoader loading={loading} size={150} color={'#f37a24'}></RingLoader>
       </div>
     ) : (
       <>
-      {!formouse ? <Typography className='tooltip_map'>{name}</Typography>: ''}
-      <h1 style={{textAlign: 'center'}}>Online Mahalla </h1>
-      {forclick ? <Dialog open={forclick} onClose={handleClose} village={village}/> : ''}
-      <Select/>
-      <YMaps >
+      <h1 style={{textAlign: 'center'}}>Online Mahalla </h1> 
+      {forclick ? <Dialog open={forclick} onClose={handleClose} village={village}/> : ''} 
+      <Select data={Points} onParam={handleParam}/>
+       <YMaps >
         <Map
           width='100vw'
           height='95vh'
           defaultState={{
-            center: [41.311151, 69.279716],
-            zoom: 12
+            center: param,
+            zoom
           }}
         >
           <Clusterer options={{  preset: 'islands#invertedVioletClusterIcons',  groupByCoordinates: false, }}  >
-            {points.map((coordinates, index) =>{
-                return( 
-                        <Placemark key={index} 
-                        onMouseEnter={()=>onstart(coordinates.name)}
-                        onMouseLeave={()=>onclose()}
-                        onClick={()=>Information(coordinates)}
-                        geometry={coordinates.param}
-                          />
-            )})}
-            <Placemark  
-                        geometry={[41.329151, 69.27916]}
-                        options={{
-                          iconLayout: 'default#image',
-                          iconImageHref: pin,
-                          iconImageSize: [40, 40], 
-                          hideIconOnBalloonOpen: false,
-                          balloonOffset: [3, -40]
-                      }} />
-          </Clusterer> 
+          {mapBool ? <Placemark  
+                        geometry={param}
+                        onClick={Information}
+                        // options={{
+                        //   iconLayout: 'default#image',
+                        //   iconImageHref: pin,
+                        //   iconImageSize: [40, 40], 
+                        //   hideIconOnBalloonOpen: false,
+                        //   balloonOffset: [3, -40]}} 
+                      /> : '' }  
+         </Clusterer> 
           <GeolocationControl options={{ float: 'left' }} />
           <TypeSelector options={{ float: 'right' }} />
           <TrafficControl options={{ float: 'right' }} />
           <RouteButton options={{ float: 'right' }} />
-          <ZoomControl options={{ float: 'left' }} />
+          <ZoomControl  options={{ 
+            zoomStep : zoom,
+            float: 'left' }} />
         </Map>
     </YMaps>
-    
-  </>
+     
+   </>
     )}
-    </>
+    </> 
   );
 }
 export default App;
--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -16,6 +16,11 @@ function PaperComponent(props) {
   );
 }
  function DraggableDialog({open, onClose, village}) {
+   const {name , viloyat, tuman, RaisFIO, RaisTel, email,
+          tel, UchasFIO, UchasTel, PosbonFIO, PosbonTel,
+         QariyalarFIO, QariyalarTel, RaisOrin1FIO, RaisOrin1Tel,
+         RaisOrin2FIO,RaisOrin2Tel, RaisOrin3FIO, RaisOrin3Tel,
+        RaisOrin4FIO,RaisOrin4Tel, kotibFIO, kotibTel} = village
   return (<Dialog
         open={open}
         onClose={onClose}
@@ -24,11 +29,32 @@ function PaperComponent(props) {
       >
         <div style={{display: 'flex', justifyContent:'flex-end'}}><Button style={{width: '10px'}} onClick={onClose} color="primary"> X </Button></div>
         <DialogTitle style={{ cursor: 'move', display: 'flex', justifyContent: 'center', padding: '0 10px 15px' }} id="draggable-dialog-title" onClose={onClose}>
-          {village.name}  
+          {name}  
         </DialogTitle>
         <DialogContent>
           <DialogContentText>
-            {village.title}
+            <div>viloyat :  {viloyat}</div>
+            <div>Tuman : {tuman}</div>
+            <div>RaisFIO: {RaisFIO}</div>
+            <div>RaisTel : {RaisTel}</div>
+            <div>Email : {email}</div>
+            <div>Tel : {tel}</div>
+            <div>UchasFIO : {UchasFIO}</div>
+            <div>UchasTel : {UchasTel}</div>
+            <div>PosbonFIO : {PosbonFIO}</div>
+            <div>PosbonTel : {PosbonTel}</div>
+            <div>QariyalarFIO : {QariyalarFIO}</div>
+            <div>QariyalarTel : {QariyalarTel}</div>
+            <div>RaisOrin1FIO : {RaisOrin1FIO}</div>
+            <div>RaisOrin1Tel : {RaisOrin1Tel}</div>
+            <div>RaisOrin2FIO : {RaisOrin2FIO}</div>
+            <div>RaisOrin2Tel : {RaisOrin2Tel}</div>
+            <div>RaisOrin3FIO : {RaisOrin3FIO}</div>
+            <div>RaisOrin3Tel : {RaisOrin3Tel}</div>
+            <div>RaisOrin4FIO : {RaisOrin4FIO}</div>
+            <div>RaisOrin4Tel : {RaisOrin4Tel}</div>
+            <diV>KotibFIO : {kotibFIO}</diV>
+            <div>KotinTel : {kotibTel}</div>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
