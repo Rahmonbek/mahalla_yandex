f0d99634a0443cda7028b85771afe195b5860e78
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-08-13 17:05:43 +0500

    new selelct insert

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React,{useState, useEffect} from 'react';
-import {YMaps, Map, Clusterer, Placemark,   TypeSelector, ZoomControl, GeolocationControl, RouteButton, TrafficControl } from 'react-yandex-maps'
+import {YMaps, Map, Clusterer, Placemark, 
+        TypeSelector, ZoomControl, GeolocationControl, RouteButton, TrafficControl } from 'react-yandex-maps'
 import { Points} from './server'
 import RingLoader from 'react-spinners/RingLoader'
 import Dialog from './components/Dialog'
-//import pin from './boy.png'
 import 'antd/dist/antd.css'
 import './App.css'
 import Select from './components/Select'
@@ -12,10 +12,12 @@ function App() {
      const [loading,setLoading]= useState(true)
      const [forclick,setforclick]= useState(false)
      const [village, setVillage] = useState('')
-     const [mapBool,setMapBool] = useState(false)
      const [param, setParam]= useState([41.311151, 69.279716])
+     const [data,setData] = useState([])
      const [zoom,setZoom] = useState(6)
+
      useEffect(()=>{
+        setData(Points)
          setLoading(false)
      },[])
 
@@ -28,18 +30,18 @@ function App() {
     const handleParam = ()=>{
       let param = localStorage.getItem('param')
       setParam(JSON.parse(param))
-      setZoom(14)
       const dataJson = localStorage.getItem('selectNeighborhood')
       const data = JSON.parse(dataJson)
-      if(data){
-        setMapBool(true)
-      }else{
-        setMapBool(false)
-      }
+      console.log(data)
     }
     const handleClose = () => {
       setforclick(false);
     };
+    const handleData = ()=>{
+      setData(JSON.parse(localStorage.getItem('data')))
+      setZoom(zoom*1.5)
+    }
+      
       return (
     <>
      {loading ? (
@@ -50,7 +52,7 @@ function App() {
       <>
       <h1 style={{textAlign: 'center'}}>Online Mahalla </h1> 
       {forclick ? <Dialog open={forclick} onClose={handleClose} village={village}/> : ''} 
-      <Select data={Points}  onParam={handleParam}/>
+      <Select data={Points} onParam={handleParam} onData={handleData}/>
        <YMaps >
         <Map
           width='100vw'
@@ -61,13 +63,19 @@ function App() {
           }}
         >
           <Clusterer options={{  preset: 'islands#invertedVioletClusterIcons',  groupByCoordinates: false, }}  >
-          {mapBool ? <Placemark  geometry={param} onClick={Information} /> : '' }  
+            {data.map((info,index)=>{
+            
+            return<Placemark
+              key={index}  
+              geometry={info.param}
+              onClick={Information}
+            />})}
          </Clusterer> 
           <GeolocationControl options={{ float: 'left' }} />
           <TypeSelector options={{ float: 'right' }} />
           <TrafficControl options={{ float: 'right' }} />
           <RouteButton options={{ float: 'right' }} />
-          <ZoomControl options={{  float: 'left' }} />
+          <ZoomControl  options={{ float: 'left' }} />
         </Map>
     </YMaps>
      
--- src/components/Select.js
+++ src/components/Select.js
@@ -5,65 +5,62 @@ import { Select, Button } from 'antd';
 const { Option } = Select;
 
 function SelectMap(props) {
-  const {onParam} = props
+  const {onParam, onData} = props
   const [data,setData]  = useState(props.data)  
   const [cities,setCities] = useState([])
   const [neighborhoods,setNeighborhood] = useState([])
-  const [citybool, setCityBool] = useState(false)
-  const [mahallabool, setMahallBool] = useState(false)
-  const [provincebool,setProcince] =  useState(true)
 
-  const AllProvinces =[ ...new Set(props.data.map(item=>item.viloyat))] 
-  //const provinces = [ ...new Set(props.data.map(item=>item.viloyat))]
+  const AllProvinces =[ ...new Set(props.data.map(item=>item.viloyat))]
   const handleProvinceChange = value => {
     const selectProvince = data.filter(item => item.viloyat === value)  
-    setData(selectProvince)
-    setCities([ ...new Set(selectProvince.map(item=>item.tuman))])
-    setCityBool(true)
-    setProcince(false)
+          setData(selectProvince)
+          setCities([ ...new Set(selectProvince.map(item=>item.tuman))])
+          localStorage.setItem('data',JSON.stringify(selectProvince))
+          onData()
   };
   const handleCityChange = value => {
     const selectCity = data.filter(item => item.tuman === value) 
-    setNeighborhood([ ...new Set(selectCity.map(item=>item.name))])
-    setData(selectCity)
-    setCityBool(false)
-    setMahallBool(true)
+          setNeighborhood([ ...new Set(selectCity.map(item=>item.name))])
+          localStorage.setItem('data',JSON.stringify(selectCity))
+          setData(selectCity)
+          onData()
   };
   
   const handleNeighborhoodsChange = value=>{
     const selectNeighborhood = data.filter(item => item.name === value)
-    localStorage.setItem('selectNeighborhood', JSON.stringify(selectNeighborhood))
-    localStorage.setItem('param',JSON.stringify(selectNeighborhood[0].param))
-    onParam()
+          localStorage.setItem('selectNeighborhood', JSON.stringify(selectNeighborhood))
+          // localStorage.setItem('param', JSON.stringify(selectNeighborhood[0].param))
+          onParam()
   }
   const Result = ()=>{
     setData(props.data)
-    localStorage.removeItem('selectNeighborhood')
+    localStorage.removeItem('data')
     localStorage.removeItem('param')
     onParam()
-    setCityBool(false)
-    setMahallBool(false)
-    setProcince(true)
+    window.location.href='/'
   }
     return (
         <div className="map_select">
-          {provincebool ? <Select 
+        <div className="map_item">
+        <Select 
            defaultValue='Viloyat'  style={{ width: 120 }} onChange={handleProvinceChange}>
                 {AllProvinces.map(province => (
                 <Option key={province}>{province}</Option>
                 ))}
-            </Select> : '' } 
-            {citybool ? <Select defaultValue='Tuman' style={{ width: 120 }} onChange={handleCityChange}>
+            </Select>
+             <Select defaultValue='Tuman' style={{ width: 120 }} onChange={handleCityChange}>
                 {cities.map(item => (
                 <Option key={item}>{item}</Option>
                 ))}
-            </Select> : ''}
-           { mahallabool ? <Select style={{ width: 120 }} defaultValue='Mahalla' onChange={handleNeighborhoodsChange}>
+            </Select> 
+            <Select style={{ width: 120 }} defaultValue='Mahalla' onChange={handleNeighborhoodsChange}>
                 {neighborhoods.map(item => (
                 <Option key={item}>{item}</Option>
                 ))}
-            </Select> : ''}
-            <Button onClick={Result}>Result</Button>
+            </Select>
+            <Button onClick={Result}>Qaytish kiritish</Button>
+
+        </div>
         </div>
     )
 }
