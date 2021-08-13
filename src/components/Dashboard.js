c55fb53872746ec793afa13f025c3289140d3b3f
Diyorbek5 <davlatovdiyorbek15@gmail.com>
2021-08-13 15:20:06 +0500

    dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import { Button } from 'antd'
 import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { TOKEN_AUTH } from '../contact';
 
@@ -11,6 +12,16 @@ const onChiqish = ()=>{
         <h1 style={{textAlign: 'center'}}>
             <Button type='primary' onClick={onChiqish}><Link to='/'>Chiqish</Link></Button>
         </h1>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     )
 }
 
