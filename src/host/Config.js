c8b278103d1880fb02caad382049191c38ecc24b
Developer <62201299+Developer06092000@users.noreply.github.com>
2021-08-15 04:11:09 +0500

    dasdasd
    dasdassd

--- src/host/Config.js
+++ src/host/Config.js
@@ -1,3 +1,18 @@
-export const getMahalla=()=>{
-    
-}
+import { httpRequest, url } from "./Host";
+
+export const getMahalla = () => {
+  var config = {
+    url: `${url}`,
+    method: "get",
+  };
+  return httpRequest(config);
+};
+
+export const createMahalla = (config) => {
+  var configs = {
+    url: `${url}`,
+    method: "post",
+    data: config,
+  };
+  return httpRequest(configs);
+};
