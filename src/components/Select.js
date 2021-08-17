742b7c0a22b5692282ef1cfc121f076af9361174
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-08-17 17:26:24 +0500

    sads dsfddf

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,117 +1,142 @@
-import React, {useState,useEffect} from 'react'
-import './CSS/select.css'
-import { Select, Button } from 'antd';
+import React, { useState, useEffect } from "react";
+import "./CSS/select.css";
+import { Select, Button } from "antd";
 
 const { Option } = Select;
 
 function SelectMap(props) {
-  const {onParam, onData, onUnnecessary} = props
-  const [data,setData]  = useState(props.data)  
-  const [cities,setCities] = useState([])
-  const [neighborhoods,setNeighborhood] = useState([])
- 
-  const [tumanlar,setTumanlar] = useState([ ...new Set(data.map(item=>item.tuman))])
-  const [mahallalar,setMahallalar] = useState([ ...new Set(data.map(item=>item.nomi))])
- 
-  const AllProvinces =[ ...new Set(props.data.map(item=>item.viloyat))]
- 
-  useEffect(()=>{
-      setCities([ ...new Set(data.map(item=>item.tuman))])
-      setNeighborhood([ ...new Set(data.map(item=>item.nomi))])
-  },[])
+  const { onParam, onData, onUnnecessary } = props;
+  const [data, setData] = useState(props.data);
+  const [cities, setCities] = useState([]);
+  const [neighborhoods, setNeighborhood] = useState([]);
 
-  const handleProvinceChange = value => {
-    var g=[]
-    for(let i = 0; i < data.length; i ++){
-      if(data[i].viloyat === value){
-        g.push(data[i])
+  const [tumanlar, setTumanlar] = useState([
+    ...new Set(data.map((item) => item.tuman)),
+  ]);
+  const [mahallalar, setMahallalar] = useState([
+    ...new Set(data.map((item) => item.nomi)),
+  ]);
+
+  const AllProvinces = [...new Set(props.data.map((item) => item.viloyat))];
+
+  useEffect(() => {
+    setCities([...new Set(data.map((item) => item.tuman))]);
+    setNeighborhood([...new Set(data.map((item) => item.nomi))]);
+  }, []);
+
+  const handleProvinceChange = (value) => {
+    var g = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].viloyat === value) {
+        g.push(data[i]);
       }
-    }  
-    if(value === "1"){
-      localStorage.setItem('data',JSON.stringify(data))
-      onUnnecessary(6)
-    }else{
-      localStorage.setItem('data',JSON.stringify(g))
-      onUnnecessary(8)
     }
-    setTumanlar(g)
-    setCities([ ...new Set(g.map(item=>item.tuman))])
-    onData()
+    if (value === "1") {
+      localStorage.setItem("data", JSON.stringify(data));
+      onUnnecessary(6);
+    } else {
+      localStorage.setItem("data", JSON.stringify(g));
+      onUnnecessary(8);
+    }
+
+    //Param set
+    localStorage.setItem("param", JSON.stringify(g[0]));
+    onParam();
+    onData();
+    //set tuman
+    setTumanlar(g);
+    setCities([...new Set(g.map((item) => item.tuman))]);
+    onData();
   };
-  
-  const handleCityChange = value => {
-    var g=[]
-    for(let i = 0; i < data.length; i++){
-      if(data[i].tuman === value){
-        g.push(data[i])
+
+  const handleCityChange = (value) => {
+    var g = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].tuman === value) {
+        g.push(data[i]);
       }
     }
-    if(value === "1"){
-      localStorage.setItem('data',JSON.stringify(tumanlar))
-      onUnnecessary(8)
-    }else{
-      localStorage.setItem('data',JSON.stringify(g))
-      onUnnecessary(12)
+    if (value === "1") {
+      localStorage.setItem("data", JSON.stringify(tumanlar));
+      onUnnecessary(8);
+    } else {
+      localStorage.setItem("data", JSON.stringify(g));
+      onUnnecessary(12);
     }
-    setMahallalar(g)
-    setNeighborhood([ ...new Set(g.map(item=>item.nomi))])
-    onData()
+    //Param set
+    localStorage.setItem("param", JSON.stringify(g[0]));
+    onParam();
+    onData();
+
+    //set mahalla
+    setMahallalar(g);
+    setNeighborhood([...new Set(g.map((item) => item.nomi))]);
+    onData();
   };
-  
-  const handleNeighborhoodsChange = value=>{
-    var g=[]
-    for(let i = 0; i < data.length; i ++){
-      if(data[i].nomi === value){
-        g.push(data[i])
+
+  const handleNeighborhoodsChange = (value) => {
+    var g = [];
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].nomi === value) {
+        g.push(data[i]);
       }
     }
-    if(value === "1"){
-      localStorage.setItem('data',JSON.stringify(mahallalar))
-        onUnnecessary(12)
-    }else{
-      localStorage.setItem('data',JSON.stringify(g))
-      onUnnecessary(15)
-      if(g.length === 1){
-        localStorage.setItem('param',JSON.stringify(g))
-        onParam()
-        onData()
-      }else{
-        onUnnecessary(6)
-      }
+    if (value === "1") {
+      localStorage.setItem("data", JSON.stringify(mahallalar));
+      onUnnecessary(12);
+    } else {
+      localStorage.setItem("data", JSON.stringify(g));
+      onUnnecessary(15);
+
+      //    //Param set
+      localStorage.setItem("param", JSON.stringify(g[0]));
+      onParam();
+      onData();
     }
-  }
-  const Result = ()=>{
-    setData(props.data)
-    localStorage.removeItem('data')
-    localStorage.removeItem('param')
-    window.location.href='/'
-  }
-    return (
-        <div className="map_select">
-        <div className="map_item">
-        <Select 
-           defaultValue='Viloyat'  style={{ width: 120 }} onChange={handleProvinceChange}>
-                <Option key={1} >Hammasi</Option>
-                {AllProvinces.map(province => (
-                <Option key={province}>{province}</Option>
-                ))}
-            </Select>
-             <Select defaultValue='Tuman' style={{ width: 120 }} onChange={handleCityChange}>
-                <Option key={1} >Hammasi</Option>
-                {cities.map(item => (
-                <Option key={item}>{item}</Option>
-                ))}
-            </Select> 
-            <Select style={{ width: 120 }} defaultValue='Mahalla' onChange={handleNeighborhoodsChange}>
-                <Option key={1}>Hammasi</Option>
-                {neighborhoods.map(item => (
-                <Option key={item}>{item}</Option>
-                ))}
-            </Select>
-            <Button onClick={Result}>Barcha mahallani ko`rish</Button>
-        </div>
-        </div>
-    )
+  };
+  const Result = () => {
+    setData(props.data);
+    localStorage.removeItem("data");
+    localStorage.removeItem("param");
+    window.location.href = "/";
+  };
+  return (
+    <div className="map_select">
+      <div className="map_item">
+        <Select
+          defaultValue="Viloyat"
+          style={{ width: 120 }}
+          onChange={handleProvinceChange}
+        >
+          <Option key={1}>Hammasi</Option>
+          {AllProvinces.map((province) => (
+            <Option key={province}>{province}</Option>
+          ))}
+        </Select>
+        <Select
+          defaultValue="Tuman"
+          style={{ width: 120 }}
+          onChange={handleCityChange}
+        >
+          <Option key={1}>Hammasi</Option>
+          {cities.map((item) => (
+            <Option key={item}>{item}</Option>
+          ))}
+        </Select>
+        <Select
+          style={{ width: 120 }}
+          defaultValue="Mahalla"
+          onChange={handleNeighborhoodsChange}
+        >
+          <Option key={1}>Hammasi</Option>
+          {neighborhoods.map((item) => (
+            <Option key={item}>{item}</Option>
+          ))}
+        </Select>
+        <Button onClick={Result}>Qaytish kiritish</Button>
+      </div>
+    </div>
+  );
 }
 
-export default SelectMap
+export default SelectMap;
