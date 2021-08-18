d49c34c76507c6a45b94b02f488bf98dbcd792f3
Muhammad Ali <solijonovmuhammadali0728@gamil.com>
2021-08-18 10:50:18 +0500

    footer and select new style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,22 +4,26 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 
 const FooterPagePro = () => {
   return (
-    <MDBFooter color="mdb-color" className="font-small">
+    <MDBFooter className="font-small">
       <MDBContainer className="text-center text-md-left">
-        <hr />
         <MDBRow className="d-flex align-items-center">
           <MDBCol md="6" lg="6" sm="12">
-            <p className="text-left grey-text text-primary">&copy; {new Date().getFullYear()} Mualliflik huquqi: IT Tower</p>
+            <p className="text-left grey-text text-primary">
+              &copy; {new Date().getFullYear()} Mualliflik huquqi: IT Tower
+            </p>
           </MDBCol>
           <MDBCol md="6" lg="6" sm="12">
             <p className="d-flex justify-content-start grey-text text-primary">
-                <a href="tel:+998999349707" style={{ textDecoration: "none" }}>
-                  <i className="fa fa-phone" /> +998 99 934 97 07
-                </a>
-                <a href="tel:+998950070650" style={{ marginLeft: "15px", textDecoration: "none" }}>
-                  <i className="fa fa-phone" /> +998 95 007 06 50
-                </a>
-              </p>
+              <a href="tel:+998999349707" style={{ textDecoration: "none" }}>
+                <i className="fa fa-phone" /> +998 99 934 97 07
+              </a>
+              <a
+                href="tel:+998950070650"
+                style={{ marginLeft: "15px", textDecoration: "none" }}
+              >
+                <i className="fa fa-phone" /> +998 95 007 06 50
+              </a>
+            </p>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
