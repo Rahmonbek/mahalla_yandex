57f94a59efd9d4fc9efc1d3526f20ca71c60154a
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-08-19 17:54:38 +0500

    svd

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 export const url="https://mahalla-api.herokuapp.com/mahalla/"
+
 export const httpRequest=(config)=>{
+console.log(config)
     return(axios({
         ...config
     }))
