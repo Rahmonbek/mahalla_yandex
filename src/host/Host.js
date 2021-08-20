6bbeadf2c2d67c86bfaa0607b08ee087518fa2bb
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2021-08-20 15:04:23 +0500

    Revert "svd"
    This reverts commit 57f94a59efd9d4fc9efc1d3526f20ca71c60154a.

--- src/host/Host.js
+++ src/host/Host.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 export const url="https://mahalla-api.herokuapp.com/mahalla/"
-
 export const httpRequest=(config)=>{
-console.log(config)
     return(axios({
         ...config
     }))
